d55555eecbd8a849641d916dbc229fbff7ea6de5
demonchyClement <clement.demonchy.ext@ipsen.com>
2017-04-27 08:53:45 +0000

    Add test for using detect_file and detect_text

--- ldig/tests/test_ldig.py
+++ ldig/tests/test_ldig.py
@@ -4,16 +4,15 @@
 # Testcase of ldig
 # This code is available under the MIT License.
 # (c)2012 Nakatani Shuyo / Cybozu Labs Inc.
-
+# (c) 2017 Demonchy Clement
+from __future__ import absolute_import, unicode_literals
 import unittest
 from ldig import ldig
+import os
 
-class TestNormalization(unittest.TestCase):
+PATH_SCRIPT = os.path.dirname(os.path.realpath(__file__))
+class TestLdig(unittest.TestCase):
     """Normalization test"""
-
-    def setUp(self):
-        pass
-
     def assertNormalize(self, org, norm):
         self.assertEqual(ldig.normalize_text(org), ("", norm, org))
 
@@ -35,7 +34,15 @@ class TestNormalization(unittest.TestCase):
         self.assertNormalize(u"Iİ", u"Ii")
         self.assertNormalize(u"Iı", u"Iı")
 
+    def testDetectLangText(self):
+        ldig_object = ldig.ldig()
+        self.assertEqual(ldig_object.detect_text("Coucou")[1],"fr")
+        self.assertEqual(ldig_object.detect_text("CABOSUN Trial of Upfront Cabozantinib in Metastatic RCC http://ht.ly/P4tr30b4tTr")[1],"en")
+
+    def testDetectLangInFile(self):
+        ldig_object = ldig.ldig()
+        prob_and_lang = ldig_object.detect_file([os.path.join(PATH_SCRIPT ,"data/tweets.txt")])
+        lang = [p_a_l[1] for p_a_l in prob_and_lang]
+        self.assertListEqual(lang,["fr","tr","fr","en","en","en","en","en","en","fr","en","en","fr","fr","fr","fr","fr","fr","sv","fr"])
 if __name__ == '__main__':
-    import sys, codecs
-    sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
     unittest.main()
